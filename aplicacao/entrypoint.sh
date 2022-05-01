3babeec659ff13c569a8cb76b587f3887247dfbf
Edvan Gomes <edvan.bgjunior@gmail.com>
2022-05-01 00:47:35 -0300

    Corrigindo porta para criar corretamente conteiner do banco, conteiner da aplicação criado

--- aplicacao/entrypoint.sh
+++ aplicacao/entrypoint.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /aplicacao/tmp/pids/server.pid
+
+rails db:setup
+rails server -b 0.0.0.0 
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
