ad44becf8bbfbc0db92b0b6dc73f30d2fc141403
Edvan Gomes <edvan.bgjunior@gmail.com>
2022-05-03 03:33:36 -0300

    Corrigindo 'entrypoint.sh' para permitir CI

--- aplicacao/entrypoint.sh
+++ aplicacao/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /aplicacao/tmp/pids/server.pid
 
-rails db:setup
-rails server -b 0.0.0.0 
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
